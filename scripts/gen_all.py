b7e649918a4bedd4957b3f221baa5d539fbbe99f
velolib <vlocitize@gmail.com>
2023-01-30 07:37:28 +0700

    Update gen_all.py script

--- scripts/gen_all.py
+++ scripts/gen_all.py
@@ -2,16 +2,27 @@
 # Used for the website
 from gregtech.flow import flow
 from pathlib import Path
+import time
+import os
+import shutil
 import typer
 
 def main(assets_path_absolute: Path):
-
+    if not assets_path_absolute.exists():
+        assets_path_absolute.mkdir()
+    else:
+        shutil.rmtree(str(assets_path_absolute))
+        time.sleep(0.1)
+        assets_path_absolute.mkdir()
     [
         flow(project.relative_to('projects'),
              create_dirs=True,
              config_path=Path('scripts/script_config.yaml'),
              output_path=assets_path_absolute)
         for project in Path('projects/').glob('**/*.yaml') if 'dev' not in str(project)
+    ]
+    [
+        project.unlink() for project in (assets_path_absolute.glob('**/*')) if project.is_file() if project.suffix == ''
     ]
 
 if __name__ == '__main__':
