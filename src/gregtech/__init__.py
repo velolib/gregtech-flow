5d38a7ae0ff130a084c14ec807fdd0abc55cbe61
velolib <vlocitize@gmail.com>
2023-02-11 17:23:28 +0700

    Remove useless imports/files

--- src/gregtech/__init__.py
+++ src/gregtech/__init__.py
@@ -6,8 +6,3 @@ YAML Driven Flowcharts for Gregtech: New Horizons
 
 https://velolib.github.io/gregtech-flow/
 """
-
-from gregtech.flow.cli import run_cli
-
-if __name__ == '__main__':
-    run_cli()
--- src/gregtech/flow/__init__.py
+++ src/gregtech/flow/__init__.py
@@ -6,5 +6,3 @@ YAML Driven Flowcharts for Gregtech: New Horizons
 
 https://velolib.github.io/gregtech-flow/
 """
-
-from gregtech.flow.wrapper import flow  # noqa
